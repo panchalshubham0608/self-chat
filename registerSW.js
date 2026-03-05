9d53ffaede7f91a43d0a2721f3e449a97e968324
Shubham Panchal <shubhampanchal@google.com>
2026-03-05 18:27:51 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/self-chat/sw.js', { scope: '/self-chat/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
