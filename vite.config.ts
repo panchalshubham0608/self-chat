57d0168098efb72469ebb2f9cb954cefa0c6382d
Shubham Panchal <shubhampanchal@google.com>
2026-03-05 19:16:51 +0530

    deploy PWA

--- vite.config.ts
+++ vite.config.ts
@@ -18,17 +18,17 @@ export default defineConfig({
         start_url: "/self-chat/",
         icons: [
           {
-            src: "/logo.png",
+            src: "/self-chat/logo.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/logo.png",
+            src: "/self-chat/logo.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/logo.png",
+            src: "/self-chat/logo.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
